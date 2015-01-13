050e848bacdfa24bfe41873176302b6c093fc3db
Timothy Guan-tin Chien <timdream@gmail.com>
2015-01-13 11:37:53 +0800

    Take matrix definition as arguments instead

--- test/chap1-2.js
+++ test/chap1-2.js
@@ -1,25 +1,19 @@
 'use strict';
 
 var HiddenMarkovModel = require('../lib/hmm');
-var HMMStateTransitionMatrix =
-  require('../lib/hmm_matrices').HMMStateTransitionMatrix;
-var HMMObservationProbabilityMatrix =
-  require('../lib/hmm_matrices').HMMObservationProbabilityMatrix;
-var HMMInitialStateDistributionMatrix =
-  require('../lib/hmm_matrices').HMMInitialStateDistributionMatrix;
 
 // (3)
-var matrixA = new HMMStateTransitionMatrix([[0.7, 0.3],
-                                            [0.4, 0.6]]);
+var a = [[0.7, 0.3],
+         [0.4, 0.6]];
 
 // (4)
-var matrixB = new HMMObservationProbabilityMatrix([[0.1, 0.4, 0.5],
-                                                   [0.7, 0.2, 0.1]]);
+var b = [[0.1, 0.4, 0.5],
+         [0.7, 0.2, 0.1]];
 
 // (5)
-var matrixPI = new HMMInitialStateDistributionMatrix([[0.6, 0.4]]);
+var pi = [[0.6, 0.4]];
 
-var hmm = new HiddenMarkovModel(matrixA, matrixB, matrixPI);
+var hmm = new HiddenMarkovModel(a, b, pi);
 
 // (6)
 var observations = [0, 1, 0, 2];
