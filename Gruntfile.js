37e9f1235f502ed30d631b7ce208801325718f6d
Timothy Guan-tin Chien <timdream@gmail.com>
2015-01-13 12:51:53 +0800

    Setup a project according to grunt-init pack

---
+++ Gruntfile.js
@@ -0,0 +1,82 @@
+"use strict";
+
+module.exports = function(grunt) {
+
+    var matchdep = require("matchdep");
+
+
+    // Configuration
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON("package.json"),
+
+        name: "<%= pkg.name %>",
+        version: "<%= pkg.version %>",
+
+
+        mainFile: "hmm.js",
+        distFile: "hmm.js",
+
+        srcDir: "lib",
+        srcFiles: "**/*.js",
+        src: "<%= srcDir %>/<%= srcFiles %>",
+
+        destDir: "dist",
+
+        jshint: {
+            options: {
+                jshintrc: ".jshintrc"
+            },
+            gruntfile: {
+                src: "Gruntfile.js"
+            },
+            src: {
+                src: ["<%= src %>"]
+            },
+            test: {
+                src: ["test/*.js"]
+            }
+        },
+
+
+
+
+
+
+        push: {
+            options: {
+                files: ["package.json"],
+                commitMessage: "Release version %VERSION%",
+                commitFiles: ["-a"],
+                tagName: "%VERSION%",
+                tagMessage: "Version %VERSION%"
+            }
+        },
+
+
+        mochacli: {
+            all: {}
+        }
+    });
+
+    // Plugins
+
+    matchdep.filterDev("grunt-*").forEach(grunt.loadNpmTasks);
+
+
+    // Tasks
+
+
+    grunt.registerTask("test", ["mochacli"]);
+    grunt.registerTask("default", ["jshint", "test"]);
+    grunt.registerTask("all", ["default"]);
+
+    grunt.registerTask("release", ["push"]);
+    grunt.registerTask("release-minor", ["push:minor"]);
+    grunt.registerTask("release-major", ["push:major"]);
+
+
+    // For Travis CI service
+    grunt.registerTask("travis", ["all"]);
+
+};
--- README.md
+++ README.md
@@ -1,6 +1,29 @@
 # A Hidden Markov Model exercise
 
-This repository is an attempt to create a useable [Hidden Markov Model](https://en.wikipedia.org/wiki/Hidden_Markov_model) library,
+[![NPM version](https://badge.fury.io/js/hmm.png)](http://badge.fury.io/js/hmm)
+[![Build Status](https://secure.travis-ci.org/timdream/hmm.png?branch=master)](http://travis-ci.org/timdream/hmm)
+[![Built with Grunt](https://cdn.gruntjs.com/builtwith.png)](http://gruntjs.com/)
+
+A simple Hidden Markov Model implementation.
+
+This repository is an attempt to create a usable [Hidden Markov Model](https://en.wikipedia.org/wiki/Hidden_Markov_model) library,
 based on the paper *A Revealing Introduction to Hidden Markov Models* by Dr. Mark Stamp of San Jose State University.
 
 The paper can be downloaded [here](http://www.cs.sjsu.edu/~stamp/RUA/HMM.pdf).
+
+## Getting Started
+Install the module with: `npm install hmm`
+
+```javascript
+var HiddenMarkovModel = require('hmm');
+// create a hmm machine with these definitions
+var hmm = new HiddenMarkovModel(aDef, bDef, piDef);
+```
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality.
+Lint and test your code using [Grunt](http://gruntjs.com/).
+
+## License
+Copyright (c) 2015 Timothy Guan-tin Chien
+Licensed under the MIT license.
